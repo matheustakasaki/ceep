28a2adb87cb9b1eba39434f6139acfcec32539d4
matheustakasaki <matheus8antunes@gmail.com>
2021-03-16 07:58:09 -0300

    Padronização dos components

---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,21 @@
+import { Component, React } from "react";
+import CardNota from "./CardNota/CardNota";
+
+class ListaDeNotas extends Component {
+    render() {
+        return (
+            <ul>
+                {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index) => {
+                    return (
+
+                        <li key={index}>
+                            <CardNota />
+                        </li>
+                    );
+                })}
+            </ul>
+        );
+    }
+}
+
+export default ListaDeNotas
